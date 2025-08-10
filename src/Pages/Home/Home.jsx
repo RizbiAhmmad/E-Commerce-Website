7f9db083842e7eae1778d259694096864fc7ac33
Rizbi ahmmad <contact.rizbi123@gmail.com>
2025-08-10 12:36:34 +0600

    Adding TopRated Product Page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,14 +3,16 @@ import Banner from './Banner';
 import ProductCard from './ProductCard';
 import PopularProduct from './PopularProduct';
 import NewArrival from './NewArrival';
+import TopRatedProduct from './TopRatedProduct';
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <ProductCard />
+            <NewArrival />
             <PopularProduct />
-            <NewArrival/>
+            <TopRatedProduct />
         </div>
     );
 };
